c435152edf7f3af406c0533812b615423dda9212
Guilherme Moraes <guilherme.moraes@outlook.com>
2017-05-16 20:58:22 -0300

    Implementing insertionSort and selectionSort

--- haskell.hs
+++ haskell.hs
@@ -7,7 +7,6 @@ collatz x
 factorial :: Integral a => a -> a
 factorial 1 = 1
 factorial x = x * factorial (x - 1)
-
 length' :: (Num b) => [a] -> b  
 length' [] = 0  
 length' (_:xs) = 1 + length' xs  
@@ -41,13 +40,22 @@ contains x (a:as)
 	| x == a = True
 	| otherwise = contains x as
 
+	
+quickSort :: (Ord a) => [a] -> [a]  
+quickSort [] = []
+quickSort (x:xs) =
+    let 
+        smallers = quickSort [s | s <- xs, s <= x]
+        greaters = quickSort [s | s <- xs, s > x]
+    in
+        smallers ++ [x] ++ greaters
 
 merge :: Ord a => [a] -> [a] -> [a]
 merge xs [] = xs
 merge [] xs = xs
 merge (x:xs) (y:ys)
-	| x <= y = x: (merge xs (y:ys))
-	| otherwise = y: (merge (x:xs) ys)
+  | x <= y = x: (merge xs (y:ys))
+  | otherwise = y: (merge (x:xs) ys)
 
 takeHalf :: [a] -> [a]
 takeHalf xs = take (div (length' xs) 2) xs
@@ -62,4 +70,35 @@ mergeSort xs =
         secondHalf = mergeSort (dropHalf xs)
     in  merge firstHalf secondHalf  
 
+
+min' :: Ord a => [a] -> a
+min' [x] = x
+min' (x:xs) = min x (min' xs)
+
+removeItemFromList :: Eq a => a -> [a] -> [a]
+removeItemFromList item xs = [ x | x <- xs, x /= item ]
+
+selectionSort :: (Ord a) => [a] -> [a]  
+selectionSort [] = []
+selectionSort xs = 
+    let
+        smallest = min' xs
+        smallestComplement = removeItemFromList smallest xs
+    in
+        smallest : selectionSort smallestComplement
+
+insertInSortedList :: (Ord a) => a -> [a] -> [a]  
+insertInSortedList x xs =
+    let 
+        smallers = [s | s <- xs, s <= x]
+        greaters = [s | s <- xs, s > x]
+    in
+        smallers ++ [x] ++ greaters
+
+insertionRecursion :: (Ord a) => [a] -> [a] -> [a]
+insertionRecursion s [] = s
+insertionRecursion ls (x:xs) = insertionRecursion (insertInSortedList x ls) xs
+
+insertionSort :: (Ord a) => [a] -> [a]
+insertionSort xs = insertionRecursion [] xs
 
