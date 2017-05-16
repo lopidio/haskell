e141bb1678e5c30d3ebd47439bd4b3fd3e1cf194
Guilherme Moraes <guilherme.moraes@outlook.com>
2017-05-16 00:20:38 -0300

    Implementing merge sort

--- haskell.hs
+++ haskell.hs
@@ -12,10 +12,12 @@ length' :: (Num b) => [a] -> b
 length' [] = 0  
 length' (_:xs) = 1 + length' xs  
 
-indexOf' :: (Eq a, Num a) => a -> [t] -> t
+indexOf' :: (Eq a, Num b) => a -> [a] -> b
 indexOf' _ [] = error "Empty list"
-indexOf' 0 (x:xs) = x
-indexOf' i (x:xs) = indexOf' (i -1) xs
+indexOf' i (x:xs) 
+	| i == x = 0
+	| null xs = error "Not found"
+	| otherwise = 1 + indexOf' i xs
 
 mod' :: Integral a => a -> a -> a
 mod' a b
@@ -28,4 +30,36 @@ isPrime k = null [ x | x <- [2 .. k - 1], k `mod'`x  == 0]
 
 extractPrimes :: Integral a => [a] -> [a]
 extractPrimes xs = [x | x <- xs, isPrime x]
+
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x]
+
+contains :: Eq a => a -> [a] -> Bool
+contains x [] = False
+contains x (a:as)
+	| x == a = True
+	| otherwise = contains x as
+
+
+merge :: Ord a => [a] -> [a] -> [a]
+merge xs [] = xs
+merge [] xs = xs
+merge (x:xs) (y:ys)
+	| x <= y = x: (merge xs (y:ys))
+	| otherwise = y: (merge (x:xs) ys)
+
+takeHalf :: [a] -> [a]
+takeHalf xs = take (div (length' xs) 2) xs
+dropHalf :: [a] -> [a]
+dropHalf xs = drop (div (length' xs) 2) xs
+
+mergeSort :: Ord t => [t] -> [t]
+mergeSort [] = []  
+mergeSort (x:[]) = [x]
+mergeSort xs = 
+    let firstHalf = mergeSort (takeHalf xs)
+        secondHalf = mergeSort (dropHalf xs)
+    in  merge firstHalf secondHalf  
+
 
