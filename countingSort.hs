21bbe20add72cc98a696dbe4aa567d047250bc4d
Guilherme Moraes <guilherme.moraes@outlook.com>
2017-05-19 00:43:15 -0300

    CountingSort almost there (does not work with repeated numbers)

--- countingSort.hs
+++ countingSort.hs
@@ -1,30 +1,52 @@
-
 allocate :: Num a => Int -> [a]
 allocate x = take x (repeat 0)
 
-
 addToIndex :: Num a => a -> Int -> [a] -> [a]
 addToIndex value idx list =
-	let
-		front = take idx list
-		element = head (drop idx list)
-		back = drop (succ idx) list
-	in
-		front ++ (value + element) : back	
+    let
+        front = take idx list
+        element = head (drop idx list)
+        back = drop (succ idx) list
+    in
+        front ++ (value + element) : back    
+
+getListIndex :: Int -> [a] -> a
+getListIndex idx list = head (drop idx list)
 
 count :: Num a => [Int] -> [a] -> [a]
 count [] to = to
 count (x:xs) to = count xs (addToIndex 1 x to)
 
-accumulate v (x:[]) = [v + x] 
-accumulate v (x:xs) = (v+x):accumulate (v + x) xs
-	
+accumulate :: Num t => t -> [t] -> [t]
+accumulate v [] = [] 
+accumulate v (x:xs) = (v+x) : (accumulate (v + x) xs)
+    
+sort :: [Int] -> [Int] -> [Int] -> [Int]
+sort accumulatedVector [] destinationVector = destinationVector
+sort accumulatedVector (x:originVector) destinationVector = 
+    let
+      index = getListIndex x accumulatedVector
+      newDestination = addToIndex x (index - 1) destinationVector
+      newAccumulatedVector = addToIndex (-1) (index - 1) accumulatedVector
+    in
+      sort accumulatedVector originVector newDestination
 
-countingSort :: Num a => [Int] -> [a]
+countingSort :: [Int] -> [Int]
 countingSort xs = 
-	let
-		zeroedVec = allocate ((maximum xs) + 1)
-		countVec = count xs zeroedVec
---		accumulate = 
-	in 
-		countVec
+    let
+        adjustValue = minimum (xs)
+        adjustedVector = 
+            if adjustValue < 0 
+            then [x - adjustValue | x <- xs]
+            else xs
+        zeroedVec = allocate ((maximum adjustedVector) + 1)
+        countVec = count adjustedVector zeroedVec
+        accumulatedVector = accumulate 0 countVec
+        sorted = sort accumulatedVector adjustedVector (allocate (length adjustedVector))
+        readjustedVector = 
+            if adjustValue < 0 
+            then [x + adjustValue | x <- sorted]
+            else sorted
+    in 
+        readjustedVector
+        
